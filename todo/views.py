aef1189fffbb94e420eb7335974311155d8e6801
ericmurray703 <ericmurray703@gmail.com>
2021-01-11 09:44:04 -0500

    Added todo model and migrated to database

--- todo/views.py
+++ todo/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError
-from django.contrib.auth import login
+from django.contrib.auth import login, logout, authenticate
 
 # Create your views here.
+def home(request):
+    return render(request, 'todo/home.html')
+
 def signupuser(request):
     if request.method == 'GET':
         return render(request, 'todo/signupuser.html', {'form': UserCreationForm()})
@@ -22,6 +25,21 @@ def signupuser(request):
             #passwords dont match
             return render(request, 'todo/signupuser.html', {'form': UserCreationForm(), 'error': 'Passwords did not match' })
 
+def loginuser(request):
+    if request.method == 'GET':
+        return render(request, 'todo/loginuser.html', {'form': AuthenticationForm()})
+    else:
+        user = authenticate(username=request.POST['username'], password=request.POST['password'])
+        if user is None: 
+            return render(request, 'todo/loginuser.html', {'form': AuthenticationForm(), 'error': 'Username and password did not match'})
+        else:
+            login(request, user)
+            return redirect('currenttodos')
+
+def logoutuser(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('home')
 
 def currenttodos(request):
         return render(request, 'todo/currenttodos.html')
--- todowoo/urls.py
+++ todowoo/urls.py
@@ -23,8 +23,11 @@ urlpatterns = [
 
     # Auth
     path('signup/', views.signupuser, name='signupuser'),
+    path('logout/', views.logoutuser, name='logoutuser'),
+    path('login/', views.loginuser, name='loginuser'),
 
     # Todos
+    path('', views.home, name='home'),
     path('current/', views.currenttodos, name='currenttodos'),
 
 ]
